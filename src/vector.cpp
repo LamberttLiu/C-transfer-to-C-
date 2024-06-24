e73c07a0bf44fc5011cc4c9bdedd09595de206cb
lambertt <lambertt1702@163.com>
2024-06-24 08:32:24 +0800

    二分查找

--- src/vector.cpp
+++ src/vector.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
+class Solution{
+public:
 
+    int GetMatchedWordCount(const vector<vector<char>>& charMatrix, const vector<string> words)
+    {
+        if(charMatrix.empty() || charMatrix[0].empty()) {
+            return 0;
+        }
+
+        vector<bool> memo(words.size(), false);
+        for(int i=0)
+
+        return 0;
+    }
+
+};
 
 
 
